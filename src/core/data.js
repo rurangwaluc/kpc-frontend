5f2428a7431c7be011e62d192ef701140149eb37
kigaliphonecenter <kigaliphonecenter@gmail.com>
2020-10-04 18:44:12 +0200

    Push to github

--- src/core/data.js
+++ src/core/data.js
@@ -5,57 +5,57 @@ export const prices = [{
   },
   {
     _id: 1,
-    name: "Rwf 0 to Rwf 99000",
+    name: "Rwf 0 to Rwf 99k",
     array: [0, 9900]
   },
   {
     _id: 2,
-    name: "Rwf 100000 to Rwf 199000",
+    name: "Rwf 100k to Rwf 199k",
     array: [1000000, 199000]
   },
   {
     _id: 3,
-    name: "Rwf 200000 to Rwf 299000",
+    name: "Rwf 200k to Rwf 299k",
     array: [200000, 299000]
   },
   {
     _id: 4,
-    name: "Rwf 300000 to Rwf 399000",
+    name: "Rwf 300k to Rwf 399k",
     array: [300000, 399000]
   },
   {
     _id: 5,
-    name: "Rwf 400000 to Rwf 499000",
+    name: "Rwf 400k to Rwf 499k",
     array: [400000, 499000]
   },
   {
     _id: 6,
-    name: "Rwf 500000 to Rwf 599000",
+    name: "Rwf 500k to Rwf 599k",
     array: [500000, 599000]
   },
   {
     _id: 7,
-    name: "Rwf 600000 to Rwf 699000",
+    name: "Rwf 600k to Rwf 699k",
     array: [600000, 699000]
   },
   {
     _id: 8,
-    name: "Rwf 700000 to Rwf 799000",
+    name: "Rwf 700k to Rwf 799k",
     array: [700000, 799000]
   },
   {
     _id: 9,
-    name: "Rwf 800000 to Rwf 899000",
+    name: "Rwf 800k to Rwf 899k",
     array: [800000, 899000]
   },
   {
     _id: 10,
-    name: "Rwf 900000 to Rwf 999000",
+    name: "Rwf 900k to Rwf 999k",
     array: [900000, 999000]
   },
   {
     _id: 11,
-    name: "More than Rwf 1000000",
+    name: " 1000000 and More",
     array: [1000000, 2000000]
   }
 ];
